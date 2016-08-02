2f773284506b4ff3c0174da3614b09d81ece3162
ZigRazor <matteobotticci@gmail.com>
2016-08-02 10:54:16 +0200

    indice utenti, utenti amministratori e delete user aggiunti

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160801103224) do
+ActiveRecord::Schema.define(version: 20160802084237) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20160801103224) do
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
     t.string   "remember_digest"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
