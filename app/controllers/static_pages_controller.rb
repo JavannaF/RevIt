1124efb0077444861111952e2c6c64de002cbf1b
ZigRazor <matteobotticci@gmail.com>
2016-08-05 16:40:28 +0200

    correzzione bug home

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
     @item = current_user.items.build if logged_in?
-    @feed_items = current_user.feed.paginate(page: params[:page])
+    @feed_items = current_user.feed.paginate(page: params[:page]) if logged_in?
   end
 
   def help
