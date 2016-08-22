3be1e96fe1ad6020c41c76ddb6fb7bba5fc8955f
ZigRazor <matteobotticci@gmail.com>
2016-08-22 12:53:05 +0200

    altre correzioni

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -62,14 +62,10 @@ class ReviewsController < ApplicationController
   # PATCH/PUT /reviews/1
   # PATCH/PUT /reviews/1.json
   def update
-    respond_to do |format|
-      if @review.update(review_params)
-        format.html { redirect_to @review, notice: 'Review was successfully updated.' }
-        format.json { render :show, status: :ok, location: @review }
-      else
-        format.html { render :edit }
-        format.json { render json: @review.errors, status: :unprocessable_entity }
-      end
+    if @review.update_attributes(review_params)
+      redirect_to @review.item
+    else
+      render 'edit'
     end
   end
 
@@ -88,8 +84,8 @@ class ReviewsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_review
-      item = Item.find(params[:id])
-      @review = item.reviews.find(params[:item_id])
+      item = Item.find(params[:item_id])
+      @review = item.reviews.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
